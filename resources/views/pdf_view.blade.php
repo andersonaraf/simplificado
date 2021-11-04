1ce2dfbe9736bcd1f63112c783353680df2c4de5
ANDERSON ARAUJO FERREIRA <anderson.araujo@riobranco.ac.gov.br>
2021-11-04 16:31:58 -0500

    testando

--- resources/views/pdf_view.blade.php
+++ resources/views/pdf_view.blade.php
@@ -70,7 +70,7 @@
         <tr>
             <td align="center">
                 <figure id="container">
-                    <img src="https://processoseletivosemsa20212.riobranco.ac.gov.br/images/caruosel/2-1634561848.png" width="90%"/>
+                    <img src="{{asset('images/semsa.jpg')}}" width="90%"/>
                 </figure>
             </td>
         </tr>
