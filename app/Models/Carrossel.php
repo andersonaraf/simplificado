416c8a5d9340c1b2b756dc976bf23f3023974899
ANDERSON ARAUJO FERREIRA <anderson.araujo@riobranco.ac.gov.br>
2021-12-08 09:27:48 -0500

    correcao

--- app/Models/Carrossel.php
+++ app/Models/Carrossel.php
@@ -3,13 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use OwenIt\Auditing\Contracts\Auditable;
-use OwenIt\Auditing\Auditable as AuditableTrait;
 
-class Carrossel extends Model implements Auditable
-{
-    //
-    use AuditableTrait;
+class Carrossel extends Model {
+
     protected $table = 'carrossel';
     protected $fillable = [
         'url_img',
