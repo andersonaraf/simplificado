e82d1788426a81ab3500de869fa5c2482be72166
jose gabriel <gabriel2007ff@hotmail.com>
2022-02-23 11:00:02 -0500

    att view

--- resources/views/usuario/area_user/view_processo_user.blade.php
+++ resources/views/usuario/area_user/view_processo_user.blade.php
@@ -20,37 +20,74 @@
                             {{--COLUNA ESQUERDA--}}
                             <div class="col-6">
                                 <label>Nome Candidato:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->name}}" disabled>
+                                <input class="form-control" type="text" value="{{strtoupper($formuser->user->name)}}" disabled>
                                 <br>
                                 <label>Data Nascimento:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->data_nascimento}}" disabled>
+                                <input class="form-control" type="text"
+                                       value="{{date('d/m/Y',strtotime($formuser->user->pessoa->data_nascimento))}}"
+                                       disabled>
                                 <br>
-                                <label>Data Nascimento:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->data_nascimento}}" disabled>
-                                <br>
-                                <label>Data Nascimento:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->data_nascimento}}" disabled>
                             </div>
                             {{--COLUNA DIREITA--}}
                             <div class="col-6">
                                 <label>CPF Candidato:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->cpf}}" disabled>
+                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->cpf}}"
+                                       disabled>
                                 <br>
-                                <label>Data Nascimento:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->data_nascimento}}" disabled>
+                                @if($formuser->user->pessoa->portador_deficiencia == 0)
+                                    <label>PNE:</label>
+                                    <input class="form-control" type="text" value="{{strtoupper('nÃo')}}" disabled>
+                                @else
+                                    <label>PNE:</label>
+                                    <input class="form-control" type="text" value="{{strtoupper('sim')}}" disabled>
+                                @endif
                                 <br>
-                                <label>Data Nascimento:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->data_nascimento}}" disabled>
-                                <br>
-                                <label>Data Nascimento:</label>
-                                <input class="form-control" type="text" value="{{$formuser->user->pessoa->data_nascimento}}" disabled>
                             </div>
-
                         </div>
                     @endforeach
-
                 </div>
+                <div class="col-12 card card-header bg-primary ">
+                    <div class="d-flex justify-content-center">
+                        <h5 class="text-white">Cargo</h5>
+                    </div>
+                </div>
+                {{--CARGO PARTICIPANTE--}}
+                <div class="card card-body">
+                    @foreach($formularioUsuario as $formuser)
+                        <div class="row">
+                            {{--COLUNA ESQUERDA--}}
+                            <div class="col-6">
+                                <label>Nome Formulário:</label>
+                                <input class="form-control" type="text" value="{{strtoupper($formuser->formulario->nome)}}"
+                                       disabled>
+                                <br>
+                                <label>Maximo Pontuação Possivel:</label>
+                                <input class="form-control" type="text"
+                                       value="{{$formuser->formulario->pontuacao}} pontos"
+                                       disabled>
+                                <br>
+                            </div>
+                            {{--COLUNA DIREITA--}}
+                            <div class="col-6">
+                                <label>Cargo:</label>
+                                <input class="form-control" type="text" value="{{strtoupper($formuser->cargo->cargo)}}" disabled>
+                                <br>
+                                <label>Escolaridade:</label>
+                                <input class="form-control" type="text" value="{{$formuser->cargo->escolaridade->nivel_escolaridade}}" disabled>
+
+                                <br>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="col-12 card card-header bg-primary ">
+                    <div class="d-flex justify-content-center">
+                        <h5 class="text-white">ANEXOS</h5>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     </div>
 
