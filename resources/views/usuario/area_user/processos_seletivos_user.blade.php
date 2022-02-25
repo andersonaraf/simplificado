b5a971aacedc49e9facf4d4cfe7fb9c5d0a57554
andersonaraf <anderson.araf@gmail.com>
2022-02-25 14:53:02 -0500

    ADICIONAR NOVA VALIDAÇÃO PARA LIBERAÇÃO DO RECURSO CASO NÃO TENHA SIDO SOLICITADO.

--- resources/views/usuario/area_user/processos_seletivos_user.blade.php
+++ resources/views/usuario/area_user/processos_seletivos_user.blade.php
@@ -31,7 +31,7 @@
                                     <a href="{{route('usuario.view.processos',[Auth::user()->id,$formuser->formulario->id])}}">
                                         <button type="button" class="btn btn-info">Vizualizar</button>
                                     </a>
-                                    @if($formuser->formulario->liberar_recurso == 1 || (strtotime($formuser->formulario->data_liberar_recurso) <= strtotime(date('Y-m-d H:i:s')) && strtotime($formuser->formulario->data_fecha_recurso) >= strtotime(date('Y-m-d H:i:s'))))
+                                    @if(is_null($formuser->recurso) && ($formuser->formulario->liberar_recurso == 1 || (strtotime($formuser->formulario->data_liberar_recurso) <= strtotime(date('Y-m-d H:i:s')) && strtotime($formuser->formulario->data_fecha_recurso) >= strtotime(date('Y-m-d H:i:s')))))
                                         <a href="{{route('usuario.recurso', $formuser->id)}}">
                                             <button type="button" class="btn btn-warning">Recurso</button>
                                         </a>
