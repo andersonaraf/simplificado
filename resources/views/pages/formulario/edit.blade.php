92f582ca8518b0467d6aeeb1d781338d85e6f775
andersonaraf <anderson.araf@gmail.com>
2022-03-08 12:19:30 -0500

    CRIAR FUNCIONALIDADE PARA ADICIONAR PDF E DESCRIÇÕES SOBE O FORMULÁRIO CRIADO.

--- resources/views/pages/formulario/edit.blade.php
+++ resources/views/pages/formulario/edit.blade.php
@@ -118,6 +118,7 @@
                             </form>
                         </div>
                     </div>
+                    @include('pages.formulario.editais.index')
                 </div>
             </main>
         </div>
---
+++ resources/views/pages/formulario/editais/index.blade.php
@@ -0,0 +1,12 @@
+<hr class="w-100">
+<div class="card">
+    <div class="card-header">
+        <h4 class="card-title font-weight-bold">LISTA DE EDITAIS</h4>
+    </div>
+    <div class="card-body">
+        <form method="post" action="#">
+            @csrf
+
+        </form>
+    </div>
+</div>
